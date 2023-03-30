bcf7f1871b4f2ce7c97aed6169745087e99c716b
Palienko.an <https://adm-itweb-bitbucket.dns-shop.ru/>
2023-03-31 02:47:55 +0600

    add configuration for jaeger

--- OpenTracingApp/Program.cs
+++ OpenTracingApp/Program.cs
@@ -1,3 +1,9 @@
+using Jaeger;
+using Jaeger.Samplers;
+using Jaeger.Senders;
+using Jaeger.Senders.Thrift;
+using OpenTracing;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +12,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddSingleton<ITracer>(
+    sp =>
+    {
+        var loggerFactory = sp.GetService<ILoggerFactory>();
+        Configuration.SenderConfiguration.DefaultSenderResolver = new SenderResolver(loggerFactory)
+            .RegisterSenderFactory<ThriftSenderFactory>();
+        var tracer = new Tracer.Builder("open-tracing-service")
+            .WithLoggerFactory(loggerFactory)
+            .WithSampler(new ProbabilisticSampler(0.1))
+            .Build();
+        return tracer;
+    });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
